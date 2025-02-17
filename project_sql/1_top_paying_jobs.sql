76b89fca38cfdb36d7aa505892c0be4d61364469
pgocotano <polgocotano@ymail.com>
2025-02-17 23:49:10 +1300

    Added files

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,24 @@
+/*
+Question: What are the top paying data analyst jobs?
+ - Identify the top 10 highest paying Data Analyst roles that are available remotely.
+ - Focus on job postings with specified salaries (remove nulls).
+ - Why? Highlight the top paying opportunities for Data Analysts
+*/
+
+select 
+    job_id,
+    job_title,
+    name as company_name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+from job_postings_fact
+left JOIN company_dim ON job_postings_fact.company_id=company_dim.company_id
+where 
+    job_title_short = 'Data Analyst' AND
+    job_location like '%New Zealand%' AND
+    salary_year_avg is not null
+order by salary_year_avg DESC
+limit 10;
+
---
+++ project_sql/2_top_paying_job_skill.sql
@@ -0,0 +1,42 @@
+/*
+Question: What skills are required for the top paying data analyst jobs?
+ - Use the top 10 highest paying jobs from the first query
+ - Add the specific skills required for these roles
+ - Why? It provides a detailed look on which high paying jobs require certain skills,
+    helping job seekers identify the skills to develop that alight with top salaries.
+
+*/
+
+
+with top_jobs as (
+    select 
+        job_id,
+        name as company_name,
+        job_location,
+        salary_year_avg
+    from job_postings_fact
+    left JOIN company_dim ON job_postings_fact.company_id=company_dim.company_id
+    where 
+        job_title_short = 'Data Analyst' AND
+        job_location like '%New Zealand%' AND
+        salary_year_avg is not null
+    order by salary_year_avg DESC
+    limit 10
+)
+
+select
+    top_jobs.*,
+    skills_dim.skills
+from top_jobs
+inner join skills_job_dim on top_jobs.job_id=skills_job_dim.job_id
+inner join skills_dim on skills_dim.skill_id=skills_job_dim.skill_id
+order by salary_year_avg DESC
+
+
+/*
+Breakdown of the most demanded skills for data analyst in 2023, based on job postings:
+SQL is the most in-demand skill, appearing in 5 job postings.
+Python, R, and Jupyter are also highly sought after, each appearing 4 times.
+Power BI, Spark, and AWS have moderate demand, showing up in 2–3 postings.
+Tableau and Snowflake appear less frequently, indicating they are required for fewer roles.
+A mix of programming, cloud, and BI tools is essential for top-paying data analyst roles.
